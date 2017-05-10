a0f248a39fa64909724fd9ee9032e1b53b28d0b7
Denise Duffy & Eli Berger & Harini Balakrishnan & Fangfei Li & Brian Wong <pair+duffydduffyd.erberger22.hariniGB.lifangfei.lil-boat@devbootcamp.com>
2017-05-10 16:42:09 -0700

    Add sessions helper

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,10 @@ color = ["red", "green", "blue"]
 symbol = ["ovals", "squiggles", "diamonds"]
 number = [1,2,3]
 shading = ["solid", "open", "lines"]
-# c - 81 combinations of color, symbol, number and shading as an array
+# c is 81 combinations of color, symbol, number and shading as an array
 c = color.product(symbol, number, shading)
 
 i = 0
-while i < c.length
   Card.create(image: "/cards/#{c[i][0][0]+c[i][1][0]+c[i][2].to_s+c[i][3][0]}.png", color: c[i][0], symbol: c[i][1], number: c[i][2], shading: c[i][3])
   i += 1
 end
