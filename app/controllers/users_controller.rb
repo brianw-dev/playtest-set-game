c052e60cbbb05bf4f8accad78e00294dd7e6781e
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-05-11 11:51:22 -0700

    Add user show page and signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.', status: 302
+      redirect_to root_path, notice: 'User was successfully created.', status: 302
     else
       render :new, status: 422
     end
+  end
+
+  def show
+    @user = User.find(session[:id])
   end
 
   private
