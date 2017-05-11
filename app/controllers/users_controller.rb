1048b94e87efaeb227ecc0287849c40e44c4ad5e
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-05-11 11:37:36 -0700

    Add sessions routes and controllers with view files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.', status: 302
+    else
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
+  end
 end
