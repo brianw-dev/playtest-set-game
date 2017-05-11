b379102f0cbeb012b312916d4fc0ab7e1aa56511
Denise Duffy & Eli Berger & Harini Balakrishnan & Fangfei Li & Brian Wong <pair+duffydduffyd.erberger22.hariniGB.lifangfei.lil-boat@devbootcamp.com>
2017-05-10 17:06:42 -0700

    Add login form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510195307) do
+ActiveRecord::Schema.define(version: 20170510232215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20170510195307) do
     t.string   "shading"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
