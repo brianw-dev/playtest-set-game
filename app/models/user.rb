ac1908708802d0ad45652eedbb5c6043b1f31d73
denise duffy <denise_duffy@denises-MacBook-Pro.local>
2017-05-11 16:07:33 -0700

    Add model validations and associations and validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   require 'bcrypt'
 
   has_secure_password
+
+  validates :first_name, :last_name, :username, :email, presence: true
+  has_many :games
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "attributes" do
+    it { should have_db_column(:first_name) }
+    it { should have_db_column(:last_name) }
+    it { should have_db_column(:username) }
+    it { should have_db_column(:email) }
+    it { should have_db_column(:password_digest) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+  end
+
+  describe "associations" do
+    it {should have_many(:games)}
+    # it {should have_many(:games).with_foreign_key('user_id').class_name('Game')}
+  end
 end
