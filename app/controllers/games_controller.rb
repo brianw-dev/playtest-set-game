cce9df983fcb3787e13b583e327c48420cb2e162
Fangfei Li <email": null>
2017-05-12 14:23:39 -0700

    Create show solution button and functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -31,29 +31,25 @@ class GamesController < ApplicationController
   def edit
     @game = Game.find(params[:id])
     @cards = display(@game)
-    p find_set(@cards)
   end
 
   def update
     @game = Game.find(params[:id])
     @cards = display(@game)
-    p find_set(@cards)
-    p "*" * 100
-    p params[:game][:add_card]
-    if params[:game][:add_card] == "3"
-      if @game.cards_shown <= 18
+    if params[:game][:add_card] == "3" && @game.cards_shown <= 18
         @game.update_attributes({cards_shown: @game.cards_shown + 3})
         @cards = display(@game)
         render :edit
-      end
+    elsif params[:game][:find_set] == "💩"
+      @solution = find_set(@cards)
+      render :edit
     elsif !@game.guess.include?(params[:game][:selected_card])
       @game.guess << params[:game][:selected_card]
       @game.save
       @guesses = convert_guesses(@game)
       if @game.guess.length == 3 && is_a_set?(@guesses)
-
         @game.cards = @game.cards - @game.guess
-        @game.update_attributes({guess: [], points: @game.points + 10})
+        @game.update_attributes({guess: [], points: @game.points + 10, cards_shown: @game.cards_shown - 3})
         flash[:notice] = "🙌 You found a set! 🎉"
         redirect_to edit_game_path(@game)
       elsif @game.guess.length >= 3
